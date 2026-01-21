285a370fbbc926b6596ec8b8eab73d45960e1e64
Nicole Sun <nsun0511@gmail.com>
2026-01-21 11:07:49 -0500

    js experimentation in class

--- jsFollowAlong/script.js
+++ jsFollowAlong/script.js
@@ -1,1 +1,14 @@
-console.log("hello");
+// console.log("hello");
+// get a single element from its id (this is why id’s must be unique!)
+// document.getElementById(“myID”);
+
+	// get an array of all the elements in a given class
+// document.getElementsByClassName(“myClass”);
+
+	// get an array of all the elements with a given tag (e.g., all the h2 elements)
+// document.getElementsByTagName(“h2”);
+
+let listItems = document.getElementsByTagName("li");
+for (let i = 0; i < listItems.length; i++) {
+    console.log(listItems.length);
+}
